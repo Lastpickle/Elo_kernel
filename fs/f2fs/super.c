1847d4a6e01d4d672ef643eb0821ac8601b25760
Sahitya Tummala <stummala@codeaurora.org>
2019-05-23 09:49:17 +0530

    f2fs: add error prints for debugging mount failure
    [ Upstream commit 9227d5227b8db354d386f592f159eaa44db1c0b8 ]
    
    Add error prints to get more details on the mount failure.
    
    Signed-off-by: Sahitya Tummala <stummala@codeaurora.org>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Stable-dep-of: 0fa4e57c1db2 ("f2fs: fix to release node block count in error path of f2fs_new_node_page()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    (cherry picked from commit 3506e1b893b5c2afa96922f36a01f018e4c4bbba)
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -3744,14 +3744,14 @@ try_onemore:
 	/* setup f2fs internal modules */
 	err = f2fs_build_segment_manager(sbi);
 	if (err) {
-		f2fs_err(sbi, "Failed to initialize F2FS segment manager (%d)",
-			 err);
+		f2fs_msg(sbi,
+			"Failed to initialize F2FS segment manager (%d)", err);
 		goto free_sm;
 	}
 	err = f2fs_build_node_manager(sbi);
 	if (err) {
-		f2fs_err(sbi, "Failed to initialize F2FS node manager (%d)",
-			 err);
+		f2fs_msg(sbi,
+			"Failed to initialize F2FS node manager (%d)", err);
 		goto free_nm;
 	}
 
